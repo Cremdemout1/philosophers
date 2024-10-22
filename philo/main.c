1bbabbe8d3fafec3514ae5202b1f5a14fed189f3
Yohan Cantin <yohancantin22@gmail.com>
2024-10-22 15:12:12 +0100

    parser fixed

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: ycantin <ycantin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 18:58:54 by ycantin           #+#    #+#             */
-/*   Updated: 2024/10/21 19:04:04 by ycantin          ###   ########.fr       */
+/*   Updated: 2024/10/22 14:41:16 by ycantin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 {
     t_table *table;
     if (argc != 5 && argc != 6)
-        return (1);
+        return (write(2, "Parser->Error: invalid input\n", 29), 1);
     table = parse_table(argv);
     if (!table)
         return (write(2, "Parser->Error: invalid input\n", 29), 1);
--- philo/parse.c
+++ philo/parse.c
@@ -6,11 +6,19 @@
 /*   By: ycantin <ycantin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 16:46:12 by ycantin           #+#    #+#             */
-/*   Updated: 2024/10/21 19:52:25 by ycantin          ###   ########.fr       */
+/*   Updated: 2024/10/22 14:39:50 by ycantin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/philo.h"
+
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
 
 bool    valid_arguments(char **arguments)
 {
@@ -20,20 +28,23 @@ bool    valid_arguments(char **arguments)
     
     if (!arguments)
         return (0);
-    j = 0;
-    while (arguments[j])
+    j = 1;
+    while (arguments[j++])
     {
         i = 0;
         len = 0;
-        if (!arguments[j] || !arguments[j][0])
+        if (!arguments[j - 1] || !arguments[j - 1][0])
             return (0);
-        while (arguments[j][i] == '0')
-            i++;
-        while (arguments[j][i++])
+        while (arguments[j - 1][i] == '0')
+                i++;
+        while (arguments[j - 1][i])
+        {
+            if (!ft_isdigit(arguments[j - 1][i++]))
+                return (0);
             len++;
+        }
         if (len >= 11)
             return (0);
-        arguments++;
     }
     return (1);
 }
@@ -60,9 +71,9 @@ t_table *parse_table(char **arguments)
 	table->all_threads_ready = 0;
     if (table->philosopher_num > INT_MAX || table->time_to_die > INT_MAX
         || table->time_to_eat > INT_MAX || table->time_to_sleep > INT_MAX
-        || table->number_of_times_to_eat > INT_MAX || table->philosopher_num < 0
-        || table->time_to_die < 0 || table->time_to_eat < 0
-        || table->time_to_sleep < 0)
-            return (NULL);
+        || table->number_of_times_to_eat > INT_MAX || table->philosopher_num <= 0
+        || table->time_to_die <= 0 || table->time_to_eat <= 0
+        || table->time_to_sleep <= 0)
+            return (free(table), NULL);
     return (table);
 }
